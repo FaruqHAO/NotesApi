5fac13c98b0236cf0674f5f68db435533c126e3e
faruq6041 <faruqod@nssmena.com>
2025-05-23 13:57:35 +0300

    program file update

--- Controllers/NotesController.cs
+++ Controllers/NotesController.cs
@@ -23,7 +23,7 @@ namespace NotesApi.Controllers
             var userId = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
 
             if (string.IsNullOrEmpty(userId))
-                return Unauthorized("User ID is missing in token.");
+                return Unauthorized("User ID is missing in token."+User.FindFirst(JwtRegisteredClaimNames.Sub));
 
             var notes = await _notesService.GetByUserIdAsync(userId);
             return Ok(notes);
